725f7ef24d810f7db6e59f4877b1f4fb772e6292
Trilby Fedoragirl <criticalglitch@users.noreply.github.com>
2025-06-18 00:34:41 -0400

    Fix folder paths for all folders

--- get_image_folders.m
+++ get_image_folders.m
@@ -1,28 +1,24 @@
-function folders = get_image_folders(destination)
-    % basefmt = '%s\\single_channel_dataset\\sep_train\\%s\\';
-    
-    sigmfCommsPath = fullfile('Radio Data', 'Training', 'CommSignal3'); % sprintf(basefmt, basePath, 'CommSignal3');
-    % sigmfEMPath    = sprintf(basefmt, basePath, 'EMISignal1');
-    % sigmfComm3CompPath  = sprintf(basefmt, basePath, 'Components\CommSignal3\Comm2');
-    % sigmfComm3InferPath = sprintf(basefmt, basePath, 'Components\CommSignal3\Interference');
-    % sigmfEM1CompPath  = sprintf(basefmt, basePath, 'Components\EMISignal1\Comm2');
-    % sigmfEM1InferPath = sprintf(basefmt, basePath, 'Components\EMISignal1\Interference');
-    
-    % pathfmt = 'Images\\%s\\%s\\';
+function folders = get_image_folders(destination)    
+    sigmfCommsPath = fullfile('Radio Data', 'Training', 'CommSignal3');
+    sigmfEMPath    = fullfile('Radio Data', 'Training', 'EMISignal1');
+    sigmfComm3CompPath  = fullfile('Radio Data', 'Training', 'Components', 'CommSignal3', 'Comm2');
+    sigmfComm3InferPath = fullfile('Radio Data', 'Training', 'Components', 'CommSignal3', 'Interference');
+    sigmfEM1CompPath  = fullfile('Radio Data', 'Training', 'Components', 'EMISignal1', 'Comm2');
+    sigmfEM1InferPath = fullfile('Radio Data', 'Training', 'Components', 'EMISignal1', 'Interference');
 
-    folderCommsPath = fullfile("Images", destination, 'CommSignal3'); % sprintf(pathfmt, destination, 'CommSignal3');
-    % folderEMPath    = sprintf(pathfmt, destination, 'EMISignal1');
-    % folderComm3CompPath  = sprintf(pathfmt, destination, 'Components\CommSignal3\Comm2');
-    % folderComm3InferPath = sprintf(pathfmt, destination, 'Components\CommSignal3\Interference');
-    % folderEM1CompPath  = sprintf(pathfmt, destination, 'Components\EMISignal1\Comm2');
-    % folderEM1InferPath = sprintf(pathfmt, destination, 'Components\EMISignal1\Interference');
+    folderCommsPath = fullfile("Images", destination, 'CommSignal3');
+    folderEMPath    = fullfile("Images", destination, 'EMISignal1');
+    folderComm3CompPath  = fullfile("Images", destination, 'Components', 'CommSignal3', 'Comm2');
+    folderComm3InferPath = fullfile("Images", destination, 'Components', 'CommSignal3', 'Interference');
+    folderEM1CompPath  = fullfile("Images", destination, 'Components', 'EMISignal1', 'Comm2');
+    folderEM1InferPath = fullfile("Images", destination, 'Components', 'EMISignal1', 'Interference');
 
     folders = [
         struct("imgPath", folderCommsPath, "dataPath", sigmfCommsPath); 
-        % struct("imgPath", folderEMPath,    "dataPath", sigmfEMPath); 
-        % struct("imgPath", folderComm3CompPath,  "dataPath", sigmfComm3CompPath); 
-        % struct("imgPath", folderComm3InferPath, "dataPath", sigmfComm3InferPath);
-        % struct("imgPath", folderEM1CompPath,  "dataPath", sigmfEM1CompPath); 
-        % struct("imgPath", folderEM1InferPath, "dataPath", sigmfEM1InferPath);
+        struct("imgPath", folderEMPath,    "dataPath", sigmfEMPath); 
+        struct("imgPath", folderComm3CompPath,  "dataPath", sigmfComm3CompPath); 
+        struct("imgPath", folderComm3InferPath, "dataPath", sigmfComm3InferPath);
+        struct("imgPath", folderEM1CompPath,  "dataPath", sigmfEM1CompPath); 
+        struct("imgPath", folderEM1InferPath, "dataPath", sigmfEM1InferPath);
     ];
 end
