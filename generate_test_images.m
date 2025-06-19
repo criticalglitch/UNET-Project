3e6b9cf889f8ffd0b7cfde50877245492065d15e
Rebecca McCullough <criticalglitch@users.noreply.github.com>
2025-06-18 22:39:38 -0400

    Generate images for the entire an entire set of radar files

--- generate_test_images.m
+++ generate_test_images.m
@@ -1,10 +1,10 @@
 function generate_test_images(testImagePath)
     fprintf("Test Images Started Generating At: %s\n", datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss Z'));
-
+    
+    alpha_files = 25;
     frameFiles = 100;
     frameLen = 128;
     set = 1;
-    alpha = 1;
     nAntenna = 4;
 
     sample_rate = 25e6;
@@ -13,14 +13,16 @@ function generate_test_images(testImagePath)
         mkdir(testImagePath);
     end
 
-    parfor fileIdx = 1:frameFiles
-        for antenna = 1:nAntenna
-            pngName = fullfile(testImagePath, sprintf("Frame %03d - Antenna %d.png", fileIdx, antenna));
-            if exist(pngName, 'file') ~= 2
-                signal_data = read_rfchallenge_multisensor_frame(frameLen, set, alpha, fileIdx);
-                channels = reshape_per_antenna(signal_data, nAntenna);
-                signalFrequency = carrier_frequency(channels(antenna), sample_rate);
-                generate_specgram_imagefile(signal_data, signalFrequency, pngName);
+    for alpha = 1:alpha_files
+        parfor fileIdx = 1:frameFiles
+            for antenna = 1:nAntenna
+                pngName = fullfile(testImagePath, sprintf("l%03ds%02da%02df%03dc%d.png", frameLen, set, alpha, fileIdx, antenna));
+                if exist(pngName, 'file') ~= 2
+                    signal_data = read_rfchallenge_multisensor_frame(frameLen, set, alpha, fileIdx);
+                    channels = reshape_per_antenna(signal_data, nAntenna);
+                    signalFrequency = carrier_frequency(channels(antenna), sample_rate);
+                    generate_specgram_imagefile(signal_data, signalFrequency, pngName);
+                end
             end
         end
     end
