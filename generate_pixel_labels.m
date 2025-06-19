aa56607143ab23ed075f657c4d376a0d4880abd4
Rebecca McCullough <criticalglitch@users.noreply.github.com>
2025-06-18 22:38:46 -0400

    Fix pixel label directory creation on Linux

--- generate_pixel_labels.m
+++ generate_pixel_labels.m
@@ -1,33 +1,28 @@
 function generate_pixel_labels(trainPath, pixelPath, imageSize, classifier)
     fprintf("Pixel Label Images Started Generating At: %s\n", datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss Z'));
     colmap = load("colormap.mat");
+    
+    % pre create destination directories
+    folders = get_image_folders("GroundTruth");
+    mkdir(pixelPath);
+    for i = 1:length(folders)
+        path = folders(i).imgPath;
+        mkdir(path);
+    end
+
     imdsTrain = imageDatastore(trainPath, IncludeSubfolders=true, LabelSource="foldernames");
     % imgResize = transform(imdsTrain, @(x) imresize(x, imageSize)); % resize images
     % imgIdx = transform(imgResize, @(x) rgb2ind(x, colmap.cm));
 
-    % pre create destination directories
-    if exist(pixelPath, "dir") ~= 7
-        mkdir(pixelPath);
-    end
-
-    % recursive directory creationu
-    folders = dir(strjoin([ trainPath "/**/*.*"], ''));
-    folders = folders([folders(:).isdir] == 1);
-    folders = folders(~ismember({folders(:).name}, {'.', '..'}));
-    for i = 1:length(folders)
-        path = [folders(i).folder '/' folders(i).name];
-        path = replace(path, trainPath, pixelPath);
-        mkdir(path);
-    end
-
     files = imdsTrain.Files;
+    cm = colmap.cm;
     parfor fileIdx = 1:length(files)
         file = string(files(fileIdx));
         destPath = string(replace(file, trainPath, pixelPath));
         if exist(destPath, 'file') ~= 2
             img = imread(file, "png");
             img = imresize(img, imageSize);
-            img = rgb2ind(img, colmap.cm);
+            img = rgb2ind(img, cm);
             [rows, cols] = size(img);
 
             dat = double(reshape(img, [], 1));
