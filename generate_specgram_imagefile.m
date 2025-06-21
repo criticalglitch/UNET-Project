8bc49ec4c4a8d998567fe282e948f26f6335bb95
Trilby Fedoragirl <criticalglitch@users.noreply.github.com>
2025-06-21 13:07:30 -0400

    Gitattr + contrast spegrams
    Add gitattributes and improve contrast of evaluation images.

--- generate_specgram_imagefile.m
+++ generate_specgram_imagefile.m
@@ -1,7 +1,17 @@
-function generate_specgram_imagefile(signal_data, carrier_frequency, save_path)
+function generate_specgram_imagefile(signal_data, carrier_frequency, save_path, window, noverlap, nfft, threshold)
+    arguments
+        signal_data (1, :) {mustBeNumeric}
+        carrier_frequency (1, 1) {mustBeNumeric, mustBePositive}
+        save_path (1, 1) {mustBeText}
+        window (1, 1) {mustBeInteger, mustBePositive} = 128
+        noverlap (1, 1) {mustBeInteger, mustBePositive} = 64
+        nfft (1, 1) {mustBeInteger, mustBePositive} = 128
+        threshold (1, 1) {mustBeNumeric} = -Inf
+    end
+
     colmap = load("colormap.mat");
 
-    spectrogram(signal_data, 128, 64, 128, carrier_frequency);
+    spectrogram(signal_data, window, noverlap, nfft, carrier_frequency, MinThreshold=threshold);
     colormap(colmap.cm);
     
     set(gca, 'XTick', []); % point = increment
