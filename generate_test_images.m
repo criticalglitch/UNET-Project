d5e76a1b4a2490bd122ee373866efd714414bea2
Rebecca McCullough <criticalglitch@users.noreply.github.com>
2025-06-22 15:49:27 -0400

    Fix different code issues from testing

--- generate_test_images.m
+++ generate_test_images.m
@@ -1,11 +1,11 @@
 function generate_test_images(testImagePath)
     fprintf("Test Images Started Generating At: %s\n", datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss Z'));
     
-    alpha_files = 25;
-    frameFiles = 100;
-    frameLen = 128;
-    set = 1;
-    nAntenna = 4;
+    alpha_files = 25; % 25
+    frameFiles = 100; % 100
+    frameLen = 128; % 128
+    setIdx = 1; % 1
+    nAntenna = 4; % 4
 
     sample_rate = 25e6;
 
@@ -16,12 +16,14 @@ function generate_test_images(testImagePath)
     for alpha = 1:alpha_files
         parfor fileIdx = 1:frameFiles
             for antenna = 1:nAntenna
-                pngName = fullfile(testImagePath, sprintf("l%03ds%02da%02df%03dc%d.png", frameLen, set, alpha, fileIdx, antenna));
+                pngName = fullfile(testImagePath, sprintf("l%03ds%02da%02df%03dc%d.png", frameLen, setIdx, alpha, fileIdx, antenna));
                 if exist(pngName, 'file') ~= 2
-                    signal_data = read_rfchallenge_multisensor_frame(frameLen, set, alpha, fileIdx);
+                    signal_data = read_rfchallenge_multisensor_frame(frameLen, setIdx, alpha, fileIdx);
                     channels = reshape_per_antenna(signal_data, nAntenna);
-                    signalFrequency = carrier_frequency(channels(antenna), sample_rate);
-                    generate_specgram_imagefile(channels(antenna), signalFrequency, pngName, 128, 64, 256, 20);
+                    signal = channels(antenna, :);
+                    signal = signal(:)';
+                    signalFrequency = carrier_frequency(signal, sample_rate);
+                    generate_specgram_imagefile(signal, signalFrequency, pngName, 128, 64, 256, -Inf); % Thresholding = 20
                 end
             end
         end
