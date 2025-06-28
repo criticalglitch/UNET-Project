9b4a317381553c22ee568cb4a10ab4595b042722
Rebecca McCullough <criticalglitch@users.noreply.github.com>
2025-06-28 13:16:48 -0400

    Update script to call correct function, and leverate its newly exposed parameters

--- runtest.sh
+++ runtest.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-/usr/bin/matlab -nodesktop -nosplash -r "run('unetTest.m'); exit;"
+declare -a testValues;
+testValues=("0.001" "0.0005" "0.0001" "0.00005" "0.00001");
+for item in "${testValues[@]}"; do
+	/usr/bin/matlab -nodesktop -nosplash -r "unetTest(LearnRate=$item); exit;"
+done
+
