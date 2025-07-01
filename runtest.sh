6ac1a3973ed5d2b84223d681cb9796c37aa6423d
Rebecca McCullough <criticalglitch@users.noreply.github.com>
2025-06-30 23:14:32 -0400

    Add ADAM specific training options + struct params
    Flesh out ADAM since it's my primary optimizer, including batch normalization, gradient threshold, and of course GPU acceleration.
    
    Also made some changes to runtest to account for adam vs sgdm/rmsprop

--- runtest.sh
+++ runtest.sh
@@ -11,12 +11,21 @@ export NVIDIA_TENSORRT=/usr/local/cuda/TensorRT
 #declare -a learnSchedValues=("piecewise", "exponential");
 #declare -a dropFactorValues=("0.1" "0.2" "0.3");
 #declare -a dropPeriodValues=("1" "5" "10");
-#declare -a momentumValues=("0.5" "0.7" "0.9");
+#declare -a momentumValues=("0.5" "0.7" "0.9"); % sgdm only
 #declare -a encoderDepthValues=("2" "3" "4");
 #declare -a numFiltersValues=("32" "64" "128");
 #declare -a filterSizeValues=("3" "4" "5");
+#declare -a gradientDecayValues=("0.5", "0.7", "0.9"); # adam only
+#declare -a squaredGradientValues=("true", "false"); # adam only
+#declare -a gradientThresholdValues=("0.1" "0.2" "0.3"); # adam only
+#declare -a gradientThresholdMethodValues=("l2norm", "l1norm", "none"); # adam only
+#declare -a epsilonValues=("1e-8", "1e-6", "1e-4"); # adam only
+#declare -a l2Values=("0.0001", "0.001", "0.01"); # adam only
+#testArray=(optimizerValues learnRateValues epochValues minibatchValues learnSchedValues dropFactorValues dropPeriodValues
+#	   encoderDepthValues numFiltersValues filterSizeValues gradientDecayValues squaredGradientValues gradientThresholdValues 
+#      gradientThresholdMethodValues epsilonValues l2Values);
 #testArray=(optimizerValues learnRateValues dropFactorValues dropPeriodValues epochValues
-#	   minibatchValues momentumValues encoderDepthValues numFiltersValues filterSizeValues);
+#	   minibatchValues momentumValues encoderDepthValues numFiltersValues filterSizeValues); % sgdm only
 testValues=("5" "10" "20");
 #optimizer="adam";
 #scheduler="piecewise";
@@ -28,5 +37,8 @@ exec > >(tee -a "$LOG_FILE") 2>&1
 # ls -l /nonexistent_directory # This error will also be logged
 for item in "${testValues[@]}"; do
 	echo "Appending run with value $item to logfile."
-	/usr/bin/matlab -nodesktop -nosplash -r "unetTest(LearnRate=0.001, MaxEpochs=$item, BatchSize=4, LearnDropFactor=0.2, LearnDropPeriod=5, Momentum=0.9, EncoderDepth=4, NumFirstEncoderFilters=64, FilterSize=3); exit;"
+	# adam only
+	/usr/bin/matlab -nodesktop -nosplash -r "unetTest(LearnRate=0.001, MaxEpochs=$item, BatchSize=4, LearnDropFactor=0.2, LearnDropPeriod=5, EncoderDepth=4, NumFirstEncoderFilters=64, FilterSize=3, GradientDecay=0.9, SquaredGradient=0.999, Epsilon=1e-8, L2=0.0001, GradientThreshold=0.1, GradientThresholdMethod='l2norm'); exit;"
+	# sgdm only
+#	/usr/bin/matlab -nodesktop -nosplash -r "unetTest(LearnRate=0.001, MaxEpochs=$item, BatchSize=4, LearnDropFactor=0.2, LearnDropPeriod=5, Momentum=0.9, EncoderDepth=4, NumFirstEncoderFilters=64, FilterSize=3); exit;"
 done
